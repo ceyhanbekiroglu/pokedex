7216d61bcaa307bd0ba643667f2b9c6476dd0085
ceyhan bekiroglu <ceyhanbekiroglu@ceyhans-MacBook-Air.local>
2022-04-23 21:50:41 +0200

    Fix PokemonList.js

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 export default class PokemonList extends Component {
 	state = {
-		url: "https://pokeapi.co/api/v2/pokemon/",
+		url: "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
 		pokemon: null,
 	}
 	async componentDidMount() {
@@ -16,12 +16,16 @@ export default class PokemonList extends Component {
 			<React.Fragment>
 				{this.state.pokemon ? (
 					<div className="row">
-						{this.state.pokemon.map((pokemon, index) => (
-							<PokemonCard key={index} name={pokemon.name} url={pokemon.url} />
+						{this.state.pokemon.map((pokemon) => (
+							<PokemonCard
+								key={pokemon.name}
+								name={pokemon.name}
+								url={pokemon.url}
+							/>
 						))}
 					</div>
 				) : (
-					<div>Loading...</div>
+					<div>Loading Pokemon</div>
 				)}
 			</React.Fragment>
 		)
