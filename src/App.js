52b4dba8357737198b842a997e1d1a67ffb22eeb
ceyhan bekiroglu <ceyhanbekiroglu@ceyhans-MacBook-Air.local>
2022-04-30 21:16:28 +0200

    Add react router to `App.js`

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react"
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import "./App.css"
 import NavBar from "./components/layout/NavBar"
 import Dashboard from "./components/layout/Dashboard"
+import Pokemon from "./components/pokemon/Pokemon"
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<NavBar />
-				<div className="container">
-					<Dashboard />
+			<Router>
+				<div className="App">
+					<NavBar />
+					<div className="container">
+						<Routes>
+							<Route exact path="/" element={<Dashboard />} />
+							<Route
+								exact
+								path="/pokemon/:pokemonIndex"
+								element={<Pokemon />}
+							/>
+						</Routes>
+					</div>
 				</div>
-			</div>
+			</Router>
 		)
 	}
 }
